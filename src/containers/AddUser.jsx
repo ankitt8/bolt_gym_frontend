9f8f347abf85c7d53c9cbf8eb185908d92f89737
Ankit Tiwari <ankit90499@gmail.com>
2020-12-03 10:50:19 +0530

    Fixed unnamed function error

--- src/containers/AddUser.jsx
+++ src/containers/AddUser.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
 import { useHistory } from 'react-router-dom';
-import { addUser as addUserToDatabase } from '../actions';
+import { addUser as addUserToDatabase, getUsers } from '../actions';
 import AddUser from '../components/AddUser';
 
 export default () => {
@@ -9,6 +9,7 @@ export default () => {
   const dispatch = useDispatch();
   const addUser = (user) => {
     dispatch(addUserToDatabase(user));
+    dispatch(getUsers());
     history.push('/');
   };
   return <AddUser addUser={addUser} />;
--- src/containers/DailyCount.jsx
+++ src/containers/DailyCount.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import DailyCount from '../components/DailyCount';
 
-export default function () {
+export default function DailyCountContainer() {
   const totalCount = useSelector((state) => state.users.users.length);
   const dailyCount = useSelector((state) => state.users.dailyCount);
   return <DailyCount totalCount={totalCount} dailyCount={dailyCount} />;
